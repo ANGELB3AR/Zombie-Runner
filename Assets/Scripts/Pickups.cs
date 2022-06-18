8506cdaf948f4368b3eca21e23ecd8123a41fd5e
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-06-18 18:43:39 -0500

    Created initial pickup prototype

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Player") { return; }
+
+        Debug.Log("Picked up");
+    }
+}
