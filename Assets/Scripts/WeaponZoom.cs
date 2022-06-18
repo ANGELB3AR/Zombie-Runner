21e8c846f1d8674fdf4ec717f35b236744979e70
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-06-18 11:22:28 -0500

    Added mouse sensitivity toggle to weapon zoom

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
+
+    [Header("Zoom In/Out Distance")]
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
 
+    [Header("Mouse Sensitivity")]
+    [SerializeField] float zoomedInSensitivity = 1f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+
+    RigidbodyFirstPersonController fpsController;
     bool zoomedInToggle = false;
+
+    void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     void Update()
     {
@@ -24,16 +37,20 @@ public class WeaponZoom : MonoBehaviour
             }
         }
     }
-
+    
     void ZoomIn()
     {
         mainCamera.fieldOfView = zoomedInFOV;
         zoomedInToggle = true;
+        fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
     }
 
     void ZoomOut()
     {
         mainCamera.fieldOfView = zoomedOutFOV;
         zoomedInToggle = false;
+        fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
     }
 }
